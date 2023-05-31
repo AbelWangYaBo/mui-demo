06c149d5590ea3083bec5f9ba6d02eff16aa06d8
abel.wang <abel.wang@merckgroup.com>
2023-05-31 16:58:47 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { SnackbarProvider, useSnackbar, enqueueSnackbar } from "notistack";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const showNotice = () => {
+    const date: string = new Date().toDateString();
+    const temp = (
+      <div
+        style={{
+          width: "360px",
+          minHeight: 120,
+        }}
+      >
+        success<span>{date}</span>
+      </div>
+    );
+    enqueueSnackbar({
+      message: temp,
+      variant: "success",
+      anchorOrigin: { vertical: "top", horizontal: "right" },
+      autoHideDuration: 5000,
+    });
+  };
 
   return (
     <>
+      <button onClick={showNotice}>anniu</button>
+      <SnackbarProvider maxSnack={4}></SnackbarProvider>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +53,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
